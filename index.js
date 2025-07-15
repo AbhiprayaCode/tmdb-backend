0492f5ea754638f94fe3fb3711ba9bb693cb396d
AlifAbhipraya <alifelangabhipraya@gmail.com>
2025-07-15 13:56:07 +0700

    port

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ console.log("MONGO_URI:", process.env.MONGO_URI);
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(process.env.PORT, () => {
-      console.log("Server running on port", process.env.PORT);
+    app.listen(8080, () => {
+      console.log("Server running on port", 8080);
     });
   })
   .catch(err => {
